1bf167b75017478b5fd5ee7b2f89b0735b41a015
Torsten Irländer <torsten@irlaender.de>
2015-10-24 15:40:22 +0200

    Set version info

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '1.0'
 
 setup(name='ringo_tag',
       version=version,
-      description="Extension for the ringo webframework",
+      description="Tag extention for the ringo webframework",
       long_description="""""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='ringo pyramid extension',
-      author='',
+      author='Torsten Irländer',
       author_email='',
       url='',
-      license='GPL',
+      license='GPL v2 or later',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
